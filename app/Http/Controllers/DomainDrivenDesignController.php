acec8e2a030fe73adec0de870b918e5ee6b97ec3
Sinh <sinh.buidinh@gmail.com>
2018-05-27 20:12:36 +0700

    Add new controller ddd

---
+++ app/Http/Controllers/DomainDrivenDesignController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DomainDrivenDesignController
+{
+    public function __construct()
+    {
+        //do something
+    }
+    
+    public function index()
+    {
+        echo "index ddd";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ddd', 'DomainDrivenDesignController@index')->name('ddd');
