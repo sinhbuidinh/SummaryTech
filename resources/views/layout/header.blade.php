9e8d7a1971a54b84e7487b3fe7f2f4e22561304f
Sinh <sinh.buidinh@gmail.com>
2018-06-02 16:18:56 +0700

    Add base layout header

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,25 @@
+<div class="w3-card-2 topnav" id="topnav" style="position: fixed; top: 0px;">
+    <div style="overflow: auto">
+        <div class="w3-bar w3-left" style="width:100%;overflow:hidden;height:44px">
+            <a href="javascript:void(0);" class="topnav-icons fa fa-menu w3-hide-large w3-left w3-bar-item w3-button" onclick="open_menu()" title="Menu"></a>
+            <a href="#default.asp" class="topnav-icons fa fa-home w3-left w3-bar-item w3-button" title="Home"></a>
+            <a class="w3-bar-item w3-button" href="#html/default.asp" title="HTML Tutorial">HTML</a>
+            <a class="w3-bar-item w3-button" href="#css/default.asp" title="CSS Tutorial">CSS</a>
+            <a class="w3-bar-item w3-button" href="#js/default.asp" title="JavaScript Tutorial">JAVASCRIPT</a>
+<!--            <a class="w3-bar-item w3-button" href="#sql_default.asp" title="SQL Tutorial">SQL</a>
+            <a class="w3-bar-item w3-button" href="#php/default.asp" title="PHP Tutorial">PHP</a>
+            <a class="w3-bar-item w3-button active" href="#bootstrap/default.asp" title="Bootstrap Tutorial">BOOTSTRAP</a>
+            <a class="w3-bar-item w3-button" href="#howto/default.asp" title="How To">HOW TO</a>
+            <a class="w3-bar-item w3-button" href="#jquery/default.asp" title="jQuery Tutorial">JQUERY</a>
+            <a class="w3-bar-item w3-button" href="#w3css/default.asp" title="W3.CSS Tutorial">W3.CSS</a>
+            <a class="w3-bar-item w3-button" href="#angular/default.asp" title="Angular Tutorial">ANGULAR</a>
+            <a class="w3-bar-item w3-button" href="#xml/default.asp" title="XML Tutorial">XML</a>
+            <a class="w3-bar-item w3-button" id="topnavbtn_tutorials" href="#javascript:void(0);" onclick="w3_open_nav(&quot;tutorials&quot;)" title="Tutorials">MORE <i class="fa fa-caret-down"></i><i class="fa fa-caret-up" style="display:none"></i></a>
+            <a href="javascript:void(0);" class="topnav-icons fa w3-right w3-bar-item w3-button" onclick="open_search(this)" title="Search W3Schools"></a>
+            <a href="javascript:void(0);" class="topnav-icons fa w3-right w3-bar-item w3-button" onclick="open_translate(this)" title="Translate W3Schools"></a>-->
+            <!--<a class="w3-bar-item w3-button w3-right" href="#forum/default.asp">FORUM</a>-->
+            <a class="w3-bar-item w3-button w3-right" id="topnavbtn_examples" href="javascript:void(0);" onclick="w3_open_nav(&quot;examples&quot;)" title="Examples">EXAMPLES <i class="fa fa-caret-down"></i><i class="fa fa-caret-up" style="display:none"></i></a>
+            <a class="w3-bar-item w3-button w3-right" id="topnavbtn_references" href="javascript:void(0);" onclick="w3_open_nav(&quot;references&quot;)" title="References">REFERENCES <i class="fa fa-caret-down"></i><i class="fa fa-caret-up" style="display:none"></i></a>
+        </div>
+    </div>
+</div>
