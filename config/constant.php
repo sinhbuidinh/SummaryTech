6b5ae0e2a5541bde3db70c311c14948b922fff00
Sinh <sinh.buidinh@gmail.com>
2018-06-09 22:05:53 +0700

    Add constant

---
+++ config/constant.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('MESSAGE_TYPE_SUCCESS') or define('MESSAGE_TYPE_SUCCESS', 'success');
+defined('MESSAGE_TYPE_ERROR') or define('MESSAGE_TYPE_ERROR', 'error');
+defined('MESSAGE_TYPE_WARNING') or define('MESSAGE_TYPE_WARNING', 'warning');
+defined('MESSAGE_TYPE_INFO') or define('MESSAGE_TYPE_INFO', 'info');
