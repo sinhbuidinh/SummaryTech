e4f1b152bae7c01fb6f7d2338b9be65080ede5a4
Sinh <sinh.buidinh@gmail.com>
2018-06-09 22:07:06 +0700

    Add validate

--- app/Services/ArticleTypeService.php
+++ app/Services/ArticleTypeService.php
@@ -22,6 +22,38 @@ class ArticleTypeService
             $data = array_merge($data, $data_form);
         }
 
+        $data['validate_result'] = $this->validateArticleTypeForm($request);
+
         return $data;
+    }
+
+    private function validateArticleTypeForm($request)
+    {
+        $result = [
+            'result' => true,
+            'message' => null
+        ];
+
+        $request_data = $request->all();
+
+        $form_data = $request_data['article_type_form']?? [];
+
+        if (empty($form_data)) {
+            $result['result'] = false;
+            $result['message'][MESSAGE_TYPE_ERROR][] = 'Not have data';
+            return $result;
+        }
+
+        if (empty($form_data['name'])) {
+            $result['result'] = false;
+            $result['message'][MESSAGE_TYPE_ERROR]['name'] = 'Not have name';
+        }
+
+        if (empty($form_data['lang_id'])) {
+            $result['result'] = false;
+            $result['message'][MESSAGE_TYPE_ERROR]['lang_id'] = 'Please choose lang';
+        }
+
+        return $result;
     }
 }
