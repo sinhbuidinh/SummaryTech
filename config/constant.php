39a216a76ebef634263e4044aab0333e01fcb712
Sinh <sinh.buidinh@gmail.com>
2018-06-30 09:14:36 +0700

    add date helper constant

--- config/constant.php
+++ config/constant.php
@@ -19,3 +19,9 @@ defined('CONFIG_ARR_VALS') or define('CONFIG_ARR_VALS', [
     CONFIG_ARR_BY_CODE,
     CONFIG_ARR_BY_NAME
 ]);
+
+defined('DATE_FORMAT_YMD') or define('DATE_FORMAT_YMD', 'Ymd');
+defined('DATE_FORMAT_YM') or define('DATE_FORMAT_YM', 'Ym');
+defined('DATE_FORMAT_YMD_SUB') or define('DATE_FORMAT_YMD_SUB', 'Y-m-d');
+defined('DATE_FORMAT_YMD_DOT') or define('DATE_FORMAT_YMD_DOT', 'Y.m.d');
+
