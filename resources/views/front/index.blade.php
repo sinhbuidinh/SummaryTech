a8fb37c556662f1559beb7f4ee810fb994348e0f
Sinh <sinh.buidinh@gmail.com>
2018-09-27 22:18:28 +0700

    Change front_base

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,25 +3,102 @@
 @section('title', 'Zona thần kinh')
 @section('custom_style')
     <link rel="stylesheet" href="{{ asset('css/zona/front.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/front/article_general.css') }}" />
 @endsection
 
 @section('content')
-    @foreach($articles as $article)
     <div id="content" class="site-content">
         <div id="primary" class="content-area">
             <main id="main" class="site-main" role="main">
-                @include('front.base_panel.article', [
-                    'id'    => data_get($article, 'id'),
-                    'type'  => data_get($article, 'column_type'),
-                    'title' => data_get($article, 'title'),
-                    'data'  => [
-                        'content'       => data_get($article, 'content'),
-                        'content_left'  => data_get($article, 'content_left'),
-                        'content_right' => data_get($article, 'content_right')
-                    ],
-                ])
+                <article>
+                    <div class="panel-content">
+                        <div class="wrap">
+                            <header class="entry-header">
+                                <h2 class="entry-title">Tóm tắt về nhà thuốc</h2>
+                            </header>
+                            <div class="entry-content row">
+                                <div class="column left">
+                                    <span>- Nhà thuốc chuyên chữa các bệnh liên quan về zona thần kinh. </span>
+                                    <br>
+                                    <span>Cam kết <b class="initial">khỏi hoàn toàn</b> 
+                                        với những bệnh nhân bị bệnh ZONA ngoài da
+                                        có xuất hiện các vết lở loét, chảy nước, đỏ rát, xấu về thẩm mỹ.
+                                    </span>
+                                    <br>
+                                    <span>
+                                        - Đặc biệt, chúng tôi khẳng định, nhà thuốc <b class="initial">LOAN THỦY</b> chúng tôi
+                                        đã chữa khỏi hoàn toàn được bệnh zona thần kinh bị biến chứng, 
+                                    </span>
+                                    <span>+ Những bệnh này bên ngoài đã hết các dấu hiệu zona bên ngoài</span>
+                                    <span>+ Vẫn bị đau nhức, buốt nhức, bỏng rát trong da</span>
+                                    <span>+ Những người bị bệnh nặng còn bị ngã khi lên cơn bị zona cắn.</span>
+                                    <br>
+                                    <h3>- Nhà thuốc chữa hoàn toàn bằng phương pháp đông y, không sử dụng bất kỳ loại thuốc tây y</h3>
+                                    <span>+ Đối với những bệnh nhân chưa sử dụng thuốc tây y, chưa sử dụng thuốc bổ thần kinh,thuốc giảm đau, khi đến với Nhà thuốc chữa bệnh nhanh và đơn giản hơn nhiều với những người đã sử dụng thuốc tây y. </span>
+                                    <span>+ Nhà thuốc không sử dụng thuốc viên không rõ nguồn gốc xuất xứ, 
+                                        sử dụng hoàn toàn thảo dược, thuốc nam, thuốc bắc, thuốc đắp bôi ngoài da. 
+                                        Khuyến khích bệnh nhân đến nhà thuốc để cảm nhận ngay lần đầu tiên chữa bệnh 
+                                        để có quyết định chính xác khi điều trị tại đây.
+                                    </span>
+                                    <span>+ Chúng tôi khẳng định và cam kết về tính chính xác thong tin đã đưa. 
+                                        Những bệnh nhân đã điều trị tại đây đều trả lời, thấy bớt bỏng rát, thuốc đắp rất mát, 
+                                        dễ chịu kết hợp với phương pháp vật lý trị liệu đạt hiệu quả qua từng ngày điều trị. 
+                                        Các bạn có thể cảm nhận được qua từng clip nhà thuốc quay lại 
+                                        đối với từng bệnh nhân đã chữa trị tại đây.
+                                    </span>
+                                    <h3>- Nhà thuốc khẳng định chữa hoàn toàn khỏi đối với những bệnh nhân bị bên zona thần kinh cắn vào mắt</h3>
+                                    <span>Không làm tổn thương vùng mắt, các chức năng của vùng mắt.</span>
+                                    <span>Điều mà đến nay y học vẫn chưa tìm được cách chữa trị đối với vùng mắt (là vùng rất nhạy cảm trên cơ thể con người).</span>
+                                    <h3>Thành tựu chữa bệnh của nhà thuốc</h3>
+                                    <span>- Nhà thuốc đã chữa khỏi đối với bệnh nhân bị cắn vào tai, phổi, bệnh nhân bị cắn vào dây thần kinh cơ tim.</span>
+                                    <span>(có những bệnh nhân khi đi chụp xquang đã bị trắng phần lớn phổi mà bệnh viên không kết luận ra bệnh) …. </span>
+                                    <span>- Phần lớn bệnh nhân đến với nhà thuốc bị căn vào dây thần kinh vùng BỤNG, LƯNG, TAY, CỔ ….</span>
+                                    <h4>Trên trang web này Nhà thuốc chỉ đưa thông tin để các bạn tin tưởng và tìm đến chữa bệnh, 
+                                        chúng tôi không post các bình luận của các bạn đã chữa bệnh khỏi 
+                                        . Vì không muốn những bệnh nhân, người nhà bệnh nhân nghỉ những comment, 
+                                        những bình luận khen ngợi là của Nhà thuốc làm mang tính chất quảng cáo. 
+                                    </h4>
+                                    <span>Lưu ý: Chúng tôi khuyến khích bệnh nhân, người nhà bệnh nhân đến tận nơi điều trị chữa bệnh</span>
+                                    <span>để cảm nhận nguồn thuốc chữa bệnh, phương pháp chữa bệnh, và hiệu quả từ lần đầu tiên chữa bệnh tại nhà thuốc chúng tôi.</span>
+                                    <span>Nhà thuốc chỉ đưa các clip đã quay bệnh nhân điều trị đã khỏi để mọi người biết và tin theo. </span>
+                                    <br>
+                                    <span>Chúng tôi có nhận chữa “KHOÁN” tức là chỉ những bệnh nhân đã quá đau, đau lâu, chữa nhiều nơi không khỏi, thì chúng tôi có thể chữa khoán tức là khỏi thì trả tiền theo thoả thuận. </span>
+                                    <span>Vì Nhà thuốc tin tưởng sẽ chữa khỏi hoàn toàn căn bệnh các bạn đang mắc phải. </span>
+                                    <h2>Lời khuyên dành cho bệnh nhân chưa có điều kiện đến nhà thuốc:</h2>
+                                    <span>- Sử dụng thuốc bổ Thần kinh, thuốc giảm đau sẽ làm cho căn bệnh phát triển mạnh hơn. </span>
+                                    <span>Bởi các bạn đang làm kích thích dây thần kinh “Hưng phấn” hơn.</span>
+                                    <span>=> làm cho virút cắn càng nhanh và mạnh hơn ở dây thần kinh đang bị “ngộ độc”.</span>
+                                    <h3>Nên nhà thuốc có một vài lời khuyên sau:</h3>
+                                    <span>+ Những bệnh nhân mới bị zona thần kinh có thể sử dụng thuốc bôi ngoài da để nhanh chóng khỏi bệnh. </span>
+                                    <span>+ Những bệnh nhân đã bị biến chứng của bệnh zona, tức là virút đã cắn sâu trong dây thần kinh bên trong như các dấu hiệu đã kể trên, thì tuyệt đối, nếu không thì hạn chế sử dụng thuốc giảm đau, thuốc bổ thần kinh. </span>
+                                    <span>+ Tuyệt đối không ăn các động vật sống dưới bùn  như: Lươn, ốc, ếch, các loại cá da trơn, cá không có vảy, cá được đánh giá là tanh;</span>
+                                    <span>+ Tuyệt đối không ăn gà, tôm sẽ gây ngứa, ảnh hưởng không tốt đến dây thần kinh.</span>
+                                    <span>+ Không uống trà, các chất gây kích thích như café …. hoặc hoá chất có dược tính kích thích thần kinh.</span>
+                                    <span>+ Không uống nước nóng, không uống thuốc bắc, thuốc nam đang còn nóng.</span>
+                                    <span>+ Không sử dụng phương pháp mồi ngải hơ, đốt vào da thịt sẽ gây việc khó chữa hơn nhiều khi các bạn tìm đến nhà thuốc.</span>
+                                    <span>+ Không tự ý chữa bệnh tại các cơ sở lấy máu bệnh nhân để chữa bệnh……</span>
+                                    <span></span>
+                                </div>
+                                <div class="column right">
+                                    <h2>Kết quả thực tế</h2>
+                                    <iframe 
+                                            width="100%" height="220" 
+                                            src="https://www.youtube.com/embed/gh820cou8NU" 
+                                            frameborder="0" allow="autoplay; encrypted-media" 
+                                            allowfullscreen></iframe>
+                                    <br>
+                                    <h2>Họp mặt sau chữa bệnh</h2>
+                                    <iframe 
+                                            width="100%" height="220" 
+                                            src="https://www.youtube.com/embed/I_sEKEH8SjU"
+                                            frameborder="0" allow="autoplay; encrypted-media"
+                                            allowfullscreen></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
             </main>
         </div>
     </div>
-    @endforeach
 @endsection
