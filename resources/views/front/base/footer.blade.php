05b507e85161a7dbc46f0c51208d2256552c143c
Sinh <sinh.buidinh@gmail.com>
2018-09-27 22:18:03 +0700

    Edit footer

--- resources/views/front/base/footer.blade.php
+++ resources/views/front/base/footer.blade.php
@@ -10,24 +10,22 @@
                             <br/> Thầy Loan, cổng chùa Cầu, Xã Mỹ Thái
                             <br/> Huyện Lạng Giang, Tỉnh Bắc Giang
                         </p>
-                        <p>
-                            <strong>Giờ làm việc:</strong>
-                            <br> <b>T2—T6:</b> 9:00–17:00
-                            <br> <b>T7 &amp; CN:</b> 11:00–15:00
-                        </p>
-						<p>
-							<strong>Nguyễn quốc Loan</strong>
-							<br>
-							<strong>Sđt:<strong> 039.290.8306
-						</p>
                     </div>
                 </section>
             </div>
             <div class="widget-column footer-widget-2">
                 <section id="text-9" class="widget widget_text">
-                    <h2 class="widget-title">Tóm tắt trang</h2>
                     <div class="textwidget">
-                        <p>Thông tin chữa trị bệnh ZONA cắn chìm</p>
+                        <p>
+                            <strong>Giờ làm việc:</strong>
+                            <br> <b>T2—T6:</b> 9:00–17:00
+                            <br> <b>T7 &amp; CN:</b> 11:00–15:00
+                        </p>
+                        <p>
+                            <strong>Nguyễn quốc Loan</strong>
+                            <br>
+                            <strong>SĐT:<strong> 039.290.8306
+                        </p>
                     </div>
                 </section>
             </div>
